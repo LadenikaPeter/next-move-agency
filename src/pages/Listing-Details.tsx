e08b7193180739561b14f303932253e8c4b29468
peter <peterladenika@gmail.com>
2024-02-23 10:12:27 +0100

    listing details done

--- src/pages/Listing-Details.tsx
+++ src/pages/Listing-Details.tsx
@@ -1,6 +1,8 @@
 import { useRouteLoaderData } from "react-router-dom";
 import { LISTINGS } from "../constants/Listings";
 import ArrowLink from "../components/Arrow-link";
+import FindHome from "../components/Find-home";
+import Footer from "../components/Footer";
 
 export default function ListingDetails() {
   const data = useRouteLoaderData("listing-detail") as {
@@ -22,8 +24,8 @@ export default function ListingDetails() {
   return (
     <>
       <section>
-        <div className={`h-[77.5vh]`}>
-          <div className="h-[77.5vh] bg-[linear-gradient(transparent,rgba(0,0,0,.55))] z-[1] absolute top-0 bottom-0 left-0 right-0"></div>
+        <div className={`h-[82vh]`}>
+          <div className="h-[82vh] bg-[linear-gradient(transparent,rgba(0,0,0,.55))] z-[1] absolute top-0 bottom-0 left-0 right-0"></div>
           <img
             src={`/listings/${data.image}.jpg`}
             className="object-cover h-full w-full"
@@ -31,22 +33,24 @@ export default function ListingDetails() {
         </div>
       </section>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex gap-20">
-          <div className="w-1/2">
-            <h1>{data.location}</h1>
-            <p>{data.amount}</p>
-            <div className="max-w-[270px]">
+        <div className="flex gap-20 pt-16 pb-24 md:flex-nowrap flex-wrap">
+          <div className="md:w-1/2 w-full text-[#054738]">
+            <h1 className="text-[clamp(44px,6vw,88px)] -tracking-[2.4px] lg:leading-[105px]">
+              {data.location}
+            </h1>
+            <p className="text-[32px] leading-[44.8px] mt-5">{data.amount}</p>
+            <div className="max-w-[270px] mt-[35px]">
               {data.quality.map((item) => {
                 return (
-                  <div className="flex  justify-between border-solid border-b-[1px] pb-1 border-b-[#fffcf8]">
-                    <p>{item.amount1}</p>
-                    <p>{item.text}</p>
+                  <div className="flex  justify-between border-solid border-b-[1px] pb-1 border-b-[#054738] py-[9px]">
+                    <p className="text-[20px]">{item.amount1}</p>
+                    <p className="text-[16px]">{item.text}</p>
                   </div>
                 );
               })}
             </div>
-            <div>
-              <div className="flex items-center justify-end gap-2">
+            <div className="mt-[35px]">
+              <div className="flex items-center justify-end gap-2 mb-5">
                 <p>Inquire today</p>
                 <ArrowLink
                   currentColor={"#054738"}
@@ -59,7 +63,7 @@ export default function ListingDetails() {
               </div>
             </div>
           </div>
-          <div className="w-1/2">
+          <div className="md:w-1/2 w-full">
             {data.showcase_images.map((item) => {
               return (
                 <img
@@ -70,6 +74,10 @@ export default function ListingDetails() {
             })}
           </div>
         </div>
+      </div>
+      <FindHome />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <Footer />
       </div>
     </>
   );
