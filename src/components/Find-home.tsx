7e66234e5894753679ef1603ea0944f5eb8ec169
peter <peterladenika@gmail.com>
2024-03-09 23:04:27 +0100

    clean up

--- src/components/Find-home.tsx
+++ src/components/Find-home.tsx
@@ -3,13 +3,15 @@ import ArrowLink from "./Arrow-link";
 export default function FindHome() {
   return (
     <div className="bg-[#054738] flex justify-center text-white ">
-      <div className="py-[128px] flex flex-col max-w-[800px] text-center gap-3">
-        <h1 className="text-[clamp(33px,6vw,88px)] md:-tracking-[2.4px] md:leading-[105px]  ">
-          Let's find your dream home.
-        </h1>
-        <div className="flex justify-center items-center gap-2">
-          <p>Get in touch</p>
-          <ArrowLink route="/contact" />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="py-[128px] flex flex-col max-w-[800px] text-center gap-3">
+          <h1 className="text-[clamp(33px,6vw,88px)] md:-tracking-[2.4px] md:leading-[105px]  ">
+            Let's find your dream home.
+          </h1>
+          <div className="flex justify-center items-center gap-2">
+            <p>Get in touch</p>
+            <ArrowLink route="/contact" />
+          </div>
         </div>
       </div>
     </div>
